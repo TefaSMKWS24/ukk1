fb09e477eadd9aaf2638ac987ef60bad90c3a42d
Chanif2003 <53119919+Chanif2003@users.noreply.github.com>
2025-01-23 13:59:12 +0700

    new file:   app/Http/Controllers/BarangController.php 	new file:   app/Http/Controllers/KasirController.php 	new file:   app/Http/Controllers/KategoriController.php 	new file:   app/Http/Controllers/PelangganController.php 	new file:   app/Http/Controllers/SupplierController.php 	new file:   app/Http/Controllers/TransaksiController.php 	modified:   database/seeders/BarangSeeder.php 	new file:   resources/views/barang/index.blade.php 	new file:   resources/views/kasir/index.blade.php 	new file:   resources/views/kategori/index.blade.php 	new file:   resources/views/pelanggan/index.blade.php 	new file:   resources/views/supplier/index.blade.php 	new file:   resources/views/transaksi/index.blade.php

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BarangSeeder extends Seeder
 {
@@ -12,6 +13,23 @@ class BarangSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'kode_barang' => 'BR001',
+                'nama_barang' => 'Sepatu',
+                'harga' => 100000,
+                'stok' => 10,
+                'kode_kategori' => 'K001',
+            ],
+            [
+                'kode_barang' => 'BR002',
+                'nama_barang' => 'Sepatu',
+                'harga' => 30000,
+                'stok' => 10,
+                'kode_kategori' => 'K002',
+            ],
+        ];
+
+        DB::table('barang')->insert($data);
     }
 }
